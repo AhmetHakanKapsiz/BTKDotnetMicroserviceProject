faf2f8a4d499c1b96393112b9577c9fa52ad8b6f
AhmetHakanKapsiz <ahmethakankapsiz@gmail.com>
2023-08-14 16:16:16 +0300

    add payment api implementation

--- BTKMicroservicesProject/BtkAkademi.Service.PaymentAPI/Program.cs
+++ BTKMicroservicesProject/BtkAkademi.Service.PaymentAPI/Program.cs
@@ -1,3 +1,8 @@
+using BtkAkademi.Service.PaymentAPI.Messaging;
+using BtkAkademi.Service.PaymentAPI.RabbitMQSender;
+using Microsoft.OpenApi.Models;
+using PaymentProcessor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<IProcessPayment, ProcessPayment>();
+
+builder.Services.AddSingleton<IRabbitMQPaymentMessageSender, RabbitMQPaymentMessageSender>();
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BtkAkademi.Service.PaymentAPI", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,12 +29,25 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BtkAkademi.Service.PaymentAPI v1"));
 }
+
+
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
---
+++ BTKMicroservicesProject/BtkAkademi.Service.PaymentAPI/RabbitMQSender/IRabbitMQPaymentMessageSender.cs
@@ -0,0 +1,9 @@
+﻿using BtkAkademi.MessageBus;
+
+namespace BtkAkademi.Service.PaymentAPI.RabbitMQSender
+{
+    public interface IRabbitMQPaymentMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage);
+    }
+}
